12542d9ac62e9b2e629862548f531f2464881778
Lucija Karan <LKaran@daenet.com>
2022-06-01 04:37:27 -0700

    First Commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTjTxaXJhjQq5niQKX-AEwR3T6a6fr8jE",
+  authDomain: "reddit--2.firebaseapp.com",
+  projectId: "reddit--2",
+  storageBucket: "reddit--2.appspot.com",
+  messagingSenderId: "292820970050",
+  appId: "1:292820970050:web:5e2d81175061a85be69bdb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
